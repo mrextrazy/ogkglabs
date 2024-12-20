b27ecab4b0533248951041fe968bbc933d40d179
roman <romandorosenko891@gmail.com>
2024-12-20 14:21:26 +0200

    zvit

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,33 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Зчитування датасету з файлу
 def read_dataset(filename):
     data = np.loadtxt(filename)
     return data
 
-# Основна функція
 def plot_dataset(data, output_file):
-    # Розділення координат X і Y
     x, y = data[:, 0], data[:, 1]
 
-    # Створення фігури з заданими розмірами
-    fig, ax = plt.subplots(figsize=(960 / 100, 540 / 100), dpi=100)  # 960x540 пікселів
+    fig, ax = plt.subplots(figsize=(960 / 100, 540 / 100), dpi=100)  
 
-    # Відображення точок
     ax.scatter(x, y, color='blue', s=10)
 
-    # Відключення осей і шкал
     ax.axis('off')
 
-    # Збереження результату
     plt.savefig(output_file, format='png', bbox_inches='tight', pad_inches=0)
     print(f"Графік збережено у файл: {output_file}")
 
-# Приклад використання
 if __name__ == "__main__":
-    input_file = "DS3.txt"  # Файл із даними
-    output_file = "output_plot.png"  # Файл для збереження графіку
+    input_file = "DS3.txt"  
+    output_file = "output_plot.png"  
 
     try:
         dataset = read_dataset(input_file)
